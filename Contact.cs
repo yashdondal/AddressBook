32d8b17e3911b90a5fade8734ee0b8c49beb7955
yashdondal <yashdondal21@gmail.com>
2022-10-18 20:06:22 +0530

    Contact Details

---
+++ Contact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+        //public string FirstName;
+        //public string LastName;
+        //public string Address;
+        //public string City;
+        //public string State;
+        //public int Zip;
+        //public string Phoneno;
+        //public string Email;
+        //public Contact(string firstName, string lastName, string address, string city, string state, int zip, string phoneno, string email)
+        //{
+        //    this.FirstName = firstName;
+        //    this.LastName = lastName;
+        //    this.Address = address;
+        //    this.City = city;
+        //    this.State = state;
+        //    this.Zip = zip;
+        //    this.Phoneno = phoneno;
+        //    this.Email = email;
+
+
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public string Address { get; set; }
+        //public string City { get; set; }
+        //public string State { get; set; }
+        //public int Zip { get; set; }
+        //public string Phoneno { get; set; }
+        //public string Email { get; set; }
+
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public string Address { get; set; }
+        //public string City { get; set; }
+        //public string State { get; set; }
+        //public int Zip { get; set; }
+        //public string Phoneno { get; set; }
+        //public string Email { get; set; }
+    }
+
+}
